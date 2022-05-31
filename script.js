22ab21b88577d483739844ad52d67d436e309200
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-05-31 13:59:51 +0300

    Выполнен урок 4

---
+++ script.js
@@ -0,0 +1,8 @@
+const stringChange = function (str) {
+    if (typeof str !== 'string') {
+        alert('аргумент не строка');
+        return;
+    }
+    str = str.trim();
+    return str.length > 30 ? str.slice(0, 30) + '...' : str;
+};
